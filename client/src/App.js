42c6eee1cd845a77e954f161a29dad5e9a2f5dc6
cosmin199 <cosmin_ionescu_email@yahoo.ro>
2021-10-04 03:41:29 -0700

    01_05

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,22 @@
 import { BrowserRouter as Router, Route } from "react-router-dom"
 import HomePage from "./pages/HomePage"
+import AboutPage from "./pages/AboutPage"
+import ArticlePage from "./pages/ArticlePage"
+import ArticlesList from "./pages/ArticlesList"
 import "./App.css"
+import NavBar from "./NavBar"
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Route path="/" component={HomePage} exact />
+        <NavBar />
+        <div id="page-body">
+          <Route path="/" component={HomePage} exact />
+          <Route path="/about" component={AboutPage} />
+          <Route path="/articles-list" component={ArticlesList} />
+          <Route path="/article" component={ArticlePage} />
+        </div>
       </div>
     </Router>
   )
---
+++ client/src/pages/AboutPage.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const AboutPage = () => {
+  return (
+    <>
+      <h2>about page</h2>
+    </>
+  )
+}
+
+export default AboutPage
