858a28cb6e0f329541269bf618a842cdef6632aa
cosmin199 <cosmin_ionescu_email@yahoo.ro>
2021-10-04 04:03:18 -0700

    01_17

--- client/src/pages/ArticlePage.js
+++ client/src/pages/ArticlePage.js
@@ -1,9 +1,18 @@
 import React from "react"
+import articleContent from "./article-content"
 
-const ArticlePage = () => {
+const ArticlePage = ({ match }) => {
+  const name = match.params.name
+  const article = articleContent.find((article) => article.name === name)
+
+  if (!article) return <h2>article does not exist</h2>
+
   return (
     <>
-      <h2>article page</h2>
+      <h2>{article.title}</h2>
+      {article.content.map((paragraph, key) => (
+        <p key={key}>{paragraph}</p>
+      ))}
     </>
   )
 }
